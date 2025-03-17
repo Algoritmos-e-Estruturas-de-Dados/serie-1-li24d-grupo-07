a2a575aa735c4d08f1842484741f7aec26c8b8f1
Jaime <marques3417@gmail.com>
2025-03-17 12:01:47 +0000

    1

--- src/main/kotlin/serie1/parte1/Arrays.kt
+++ src/main/kotlin/serie1/parte1/Arrays.kt
@@ -1,7 +1,19 @@
 package parte1
 
 fun findMinDifference(elem1: IntArray, elem2: IntArray): Int {
-    throw UnsupportedOperationException()
+    var minDif = Int.MAX_VALUE
+    var dif : Int = 0
+    if (elem1.count() == 0 || elem2.count() == 0) {
+        return -1
+    }
+    for (i in elem1.indices) {
+        for (j in elem2.indices) {
+            dif = kotlin.math.abs(elem1[i] - elem2[j])
+            minDif = kotlin.math.min(minDif, dif)
+        }
+    }
+
+    return minDif
 }
 
 fun counter(array: IntArray, k: Int, lower: Int, upper: Int): Pair<Int, Int> {
